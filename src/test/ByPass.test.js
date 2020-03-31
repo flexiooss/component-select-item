26cf5ab4d3ccaa7ac0a8153f72a46bbe4f030da5
Bruno FL <follet-bruno@hotmail.fr>
2020-03-31 13:58:36 +0200

    remove fake test

--- src/test/ByPass.test.js
+++ src/test/ByPass.test.js
@@ -1,30 +1,12 @@
 /* global runTest */
 import {TestCase} from 'code-altimeter-js'
-import {ComponentSelectItemBuilder} from '../../index'
-import {ViewListHandlerMounter} from '@flexio-oss/component-list-handler'
 
 const assert = require('assert')
 
 export class ByPass extends TestCase {
 
   test() {
-    const select = ComponentSelectItemBuilder.builder()
-      .application(application)
-      .parentNode(node)
-      .storeItems(store)
-      .viewListHandlerMounter(new ViewListHandlerMounter())
-      .onCreateItems((context, nodes) => {
-        nodes.forEach(node =>{
-          createViewContainer(context, node)
-            ... DO INTELLIGENT THINGS ...
-        })
-      })
-      .viewContainerSelectItemBuilder(new ViewContainerSelectItemBuilder())
-      .build()
 
-    select.actionSelect().listenWithCallback(action =>{
-      const id = action.id()
-    }, myContext)
   }
 }
 
